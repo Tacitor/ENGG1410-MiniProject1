479401a80acdcf3576f4ef0d89b144549fbacdcf
Tacitor <lukasjkrampitz@gmail.com>
2022-10-31 19:44:16 -0400

    change it to just copy the file to an new data file as output

--- JavaTestMiniProj1/src/javatestminiproj1/JavaTestMiniProj1.java
+++ JavaTestMiniProj1/src/javatestminiproj1/JavaTestMiniProj1.java
@@ -7,6 +7,7 @@ package javatestminiproj1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
@@ -19,17 +20,30 @@ public class JavaTestMiniProj1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        boolean encrytMode = true; //whether or not the program is encrypting or decrypting
+        String inputAddress = "src/javaTestMiniProj1/testFile.txt";
+        String outputAddress;
 
         //Read in the data file
         try {
             //Setup the file and scanner
-            File file = new File("src/javaTestMiniProj1/testFile.txt");
-            Scanner scanner = new Scanner(file);
-            
-            //print out the data file to the output
+            File inputFile = new File(inputAddress);
+            Scanner scanner = new Scanner(inputFile);
+
+            //set the output adress as the same as the input but with a different file extension
+            outputAddress = inputAddress.substring(0, inputAddress.indexOf('.')) + ".crp";
+            //create a new output file
+            File outputFile = new File(outputAddress);
+            //create a print wrtitter to write to the file
+            PrintWriter outputFileWriter = new PrintWriter(outputFile);
+
+            //write to the file
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                outputFileWriter.println(scanner.nextLine());
             }
+            
+            //close the file
+            outputFileWriter.close();
 
         } catch (FileNotFoundException e) {
             System.out.println("File ERROR: \n" + e);
